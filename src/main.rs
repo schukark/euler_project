01d7ab64e7a71659823fa0d07e7463d935a69718
Arckadiy Shchukin <schukark2004@gmail.com>
2024-07-09 00:39:32 +0300

    Solved 1-8, going to sleep

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod probs_1_50;
+
+fn main() {
+    // println!("Problem 1: {}", probs_1_50::prob_1_compute(1000));
+    // println!("Problem 2: {}", probs_1_50::prob_2_compute());
+    // println!("Problem 3: {}", probs_1_50::prob_3_compute(600851475143));
+    // println!("Problem 4: {}", probs_1_50::prob_4_compute(3));
+    // println!("Problem 5: {}", probs_1_50::prob_5_compute(20));
+    // println!("Problem 6: {}", probs_1_50::prob_6_compute(100));
+    // println!("Problem 7: {}", probs_1_50::prob_7_compute(10001));
+    println!("Problem 8: {}", probs_1_50::prob_8_compute());
+
+
+}
