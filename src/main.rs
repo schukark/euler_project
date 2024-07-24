e2fa6e2f6b9ed900fc27be7dbb8ea17827838866
Arckadiy Shchukin <schukark2004@gmail.com>
2024-07-25 00:43:49 +0300

    add(group 6)

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ fn main() {
         51..=75 => problems::group_3::solve(problem_number),
         76..=100 => problems::group_4::solve(problem_number),
         101..=125 => problems::group_5::solve(problem_number),
+        126..=150 => problems::group_6::solve(problem_number),
         _ => println!("Incorrect config"),
     };
 }
--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -3,3 +3,4 @@ pub mod group_2;
 pub mod group_3;
 pub mod group_4;
 pub mod group_5;
+pub mod group_6;
