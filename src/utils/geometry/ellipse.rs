045188d10f0782e3d47cd60f805987d2f079fa84
Arckadiy Shchukin <schukark2004@gmail.com>
2024-07-28 23:28:21 +0400

    added(ellipse and line intersection function)

--- src/utils/geometry/ellipse.rs
+++ src/utils/geometry/ellipse.rs
@@ -29,8 +29,48 @@ impl Ellipse {
 
     pub fn intersect_line(&self, line: Line) -> Vec<Point> {
         // ax + by + c = 0
-        // (x-x_0)^2 / a^2 + ()
-        unimplemented!()
+        // (x-x_0)^2 / a^2 + ((-c-ax)/b-y_0)^2/b^2=1
+        // (x^2 - 2x*x_0 + x_0^2) / a^2 + (c^2 / b^2 + 2acx / b^2 + a^2x^2 / b^2 - 2y_0 * (-c - ax) / b + y_0^2)^2 / b^2 = 1
+        //
+        // x^2 (a^2 / b^4 + 1 / a^2) + x * (-2x_0 / a^2 + 2ac / b^4 + 2ay_0 / b^3) +
+        // + (x_0^2 / a^2 + c^2 / b^4 + 2cy_0 / b^3 + y_0^2 / b^2) = 0 | * a^2b^4
+
+        let a = line.a;
+        let b = line.b;
+        let c = line.c;
+        let x_0 = self.center.x;
+        let y_0 = self.center.y;
+
+        let a_coef = a.powf(4.0) + b.powf(4.0);
+        let b_coef = -2.0 * x_0 * b.powf(4.0) + 2.0 * a.powf(3.0) * c + 2.0 * a.powf(3.0) * b * y_0;
+        let c_coef = x_0.powf(2.0) * b.powf(4.0)
+            + (a * c).powf(2.0)
+            + 2.0 * y_0 * a.powf(2.0) * b * c
+            + (a * b * y_0).powf(2.0);
+
+        let (a, b, c) = (a_coef, b_coef, c_coef);
+
+        if f64::abs(a) < 1e-6 {
+            let x = -c / b;
+            let y = (-c - a * x) / b;
+
+            vec![Point::from_coords(x, y)]
+        } else {
+            let discriminant = b.powf(2.0) - 4.0 * a * c;
+            let x_values = [
+                (-b + discriminant.sqrt()) / 2.0 / a,
+                (-b - discriminant.sqrt()) / 2.0 / a,
+            ];
+            let y_values = x_values
+                .iter()
+                .map(|x| (-c - a * x) / b)
+                .collect::<Vec<_>>();
+
+            vec![
+                Point::from_coords(x_values[0], y_values[0]),
+                Point::from_coords(x_values[1], y_values[1]),
+            ]
+        }
     }
 }
 
